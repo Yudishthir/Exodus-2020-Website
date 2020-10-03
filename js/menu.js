3442c6227076353f9fbf5a3430f17ff07ef78ba2
Kushagr-Sharma <kushagr12345@gmail.com>
2020-10-03 21:33:36 +0530

    contact done

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function hamburger() {
+  var x = document.getElementsByClassName("web-nav-list");
+  x[0].style.display = "flex";
+  x[0].style.flexDirection = "column";
+  document.getElementById("active").style.border = "none"
+  x[0].style.width = "100vw";
+  x[0].style.justifyItems = "center";
+  x[0].style.alignItems = "center";
+}
